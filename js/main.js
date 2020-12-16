916b5bdf157812d8d0b747af3d6befc7e05bc762
tomnkgw <tomnkgw@gmail.com>
2020-12-16 16:19:37 +0900

    最終版

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-// コメント入れた
 const FizzBuzz =()=> {
-  for(let i =1; i< 101; i++) {
+  const div1 = document.getElementById('div1')
+  for(let i =1 ; i< 101; i++) {
     if(i % 3 == 0 && i % 5 == 0) {
-      console.log('FizzBuzz');
+      let p1 = document.createElement("p")
+      p1.appendChild(document.createTextNode("FizzBuzz"))
+      div1.appendChild(p1)
     } else if(i % 5 == 0) {
-      console.log('Buzz');
+      p1 = document.createElement("p")
+      p1.appendChild(document.createTextNode("Buzz"))
+      div1.appendChild(p1)
     } else if (i % 3 == 0){
-      console.log('Fizz')
+      p1 = document.createElement("p")
+      p1.appendChild(document.createTextNode("Fizz"))
+      div1.appendChild(p1)
     } else {
-      console.log(i)
+      p1 = document.createElement("p")
+      p1.appendChild(document.createTextNode(i))
+      div1.appendChild(p1)
     }
   }
 }
+
+FizzBuzz()
+
+
+
